7a03d71919cd983ccee2513fd55046e1239f711c
Stéphane Lecorney <stephane.lecorney@heig-vd.ch>
2021-11-24 14:19:02 +0100

    fix db migration syntax

--- db/migrations/20211118140000-geometadata-update.js
+++ db/migrations/20211118140000-geometadata-update.js
@@ -3,31 +3,25 @@
 module.exports = {
   up: async (queryInterface) => {
     await queryInterface.sequelize.query(`
-      ALTER TABLE geometadata DROP COLUMNS (
-        git_revision_hash
-      );
-      ALTER TABLE geometadata ADD COLUMNS (
-        viewshed_git_revision_hash BYTEA DEFAULT NULL,
-        toponyms_iterations INTEGER DEFAULT NULL,
-        toponyms_count numeric[] DEFAULT NULL,
-        toponyms_factors numeric[] DEFAULT NULL,
-        toponyms_git_revision_hash BYTEA DEFAULT NULL
-      );
+      ALTER TABLE geometadata DROP COLUMN git_revision_hash;
+      ALTER TABLE geometadata
+        ADD COLUMN viewshed_git_revision_hash BYTEA DEFAULT NULL,
+        ADD COLUMN toponyms_iterations INTEGER DEFAULT NULL,
+        ADD COLUMN toponyms_count numeric[] DEFAULT NULL,
+        ADD COLUMN toponyms_factors numeric[] DEFAULT NULL,
+        ADD COLUMN toponyms_git_revision_hash BYTEA DEFAULT NULL;
     `);
   },
 
   down: async (queryInterface) => {
     await queryInterface.sequelize.query(`
-      ALTER TABLE geometadata ADD COLUMNS (
-        git_revision_hash BYTEA DEFAULT NULL
-      );
-      ALTER TABLE geometadata DROP COLUMNS (
-        viewshed_git_revision_hash,
-        toponyms_iterations,
-        toponyms_count,
-        toponyms_factors,
-        toponyms_git_revision_hash
-      );
+      ALTER TABLE geometadata ADD COLUMN git_revision_hash BYTEA DEFAULT NULL;
+      ALTER TABLE geometadata
+        DROP COLUMN viewshed_git_revision_hash,
+        DROP COLUMN toponyms_iterations,
+        DROP COLUMN toponyms_count,
+        DROP COLUMN toponyms_factors,
+        DROP COLUMN toponyms_git_revision_hash;
     `);
   }
 };
