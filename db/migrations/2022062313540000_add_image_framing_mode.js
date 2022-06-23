c98a5468cf2b14f4cb7809de0d65aa89c72e40b2
nicolas-heigvd <nicolas.blanc1@heig-vd.ch>
2022-06-23 13:59:15 +0200

    Update image table; set not null and add default value to the framing_mode field and fill it where it was null.

---
+++ db/migrations/2022062313540000_add_image_framing_mode.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const { irreversibleMigration } = require('../utils');
+
+module.exports = {
+  up: async (queryInterface) => {
+    // collection_id = 36 := SARI
+    await queryInterface.sequelize.query(`
+        UPDATE images SET framing_mode = 'single_image' WHERE framing_mode IS NULL;
+        UPDATE images SET framing_mode = 'composite_image' WHERE collection_id = 36;
+    `);
+
+    await queryInterface.sequelize.query(`
+        ALTER TABLE images ALTER COLUMN framing_mode SET NOT NULL;
+        ALTER TABLE images ALTER COLUMN framing_mode SET DEFAULT 'single_image';
+    `);
+  },
+
+  down: irreversibleMigration(__filename)
+
+};
