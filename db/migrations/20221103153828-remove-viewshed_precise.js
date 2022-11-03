e0c6d6b0311f827721400d6b597597bbc431a320
nicolas-heigvd <nicolas.blanc1@heig-vd.ch>
2022-11-03 15:43:37 +0100

    Remove viewshed_precise from db model; both in the images and geometadata tables.

---
+++ db/migrations/20221103153828-remove-viewshed_precise.js
@@ -0,0 +1,27 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface) => {
+    await queryInterface.sequelize.query(`
+        ALTER TABLE public.geometadata
+        DELETE COLUMN viewshed_precise;
+    `);
+
+    await queryInterface.sequelize.query(`
+        ALTER TABLE public.images
+        DELETE COLUMN viewshed_precise;
+    `);
+  },
+  
+  down: async (queryInterface) => {
+    await queryInterface.sequelize.query(`
+      ALTER TABLE public.geometadata
+      ADD COLUMN viewshed_precise Geometry('MultiPolygon',4326,2) DEFAULT NULL;
+    `);
+
+    await queryInterface.sequelize.query(`
+      ALTER TABLE public.images
+      ADD COLUMN viewshed_precise Geometry('MultiPolygon',4326,2) DEFAULT NULL;
+    `);
+  }
+};
