710d7542d38ce18fba58c8e645e3ffb0ebc2e3b5
James Taylor <james.taylor@heig-vd.ch>
2025-05-12 11:53:09 +0200

    changing back to single quotes from last commit

--- app/api/images/images.list.controller.js
+++ app/api/images/images.list.controller.js
@@ -1,10 +1,10 @@
-const Sequelize = require("sequelize");
-const { intersection } = require("lodash");
+const Sequelize = require('sequelize');
+const { intersection } = require('lodash');
 
-const models = require("../../models");
-const { userHasRole } = require("../../utils/authorization");
-const { authorizationError } = require("../../utils/errors");
-const utils = require("../../utils/express");
+const models = require('../../models');
+const { userHasRole } = require('../../utils/authorization');
+const { authorizationError } = require('../../utils/errors');
+const utils = require('../../utils/express');
 const {
   inUniqueOrList,
   containsUniqueOrList,
@@ -13,33 +13,33 @@ const {
   iLikeFormatter,
   cleanProp,
   toUniqueArray,
-} = require("../../utils/params");
-const { getOwnerScope } = require("./images.utils");
-const mediaUtils = require("../../utils/media");
+} = require('../../utils/params');
+const { getOwnerScope } = require('./images.utils');
+const mediaUtils = require('../../utils/media');
 
 const Op = Sequelize.Op;
 
 const parseAttributes = (query) => {
   const basic_attributes = [
-    "id",
-    "original_id",
-    "collection_id",
-    "owner_id",
-    "title",
-    "is_published",
-    "state",
-    "date_georef",
-    "height",
-    "width",
-    "iiif_data",
+    'id',
+    'original_id',
+    'collection_id',
+    'owner_id',
+    'title',
+    'is_published',
+    'state',
+    'date_georef',
+    'height',
+    'width',
+    'iiif_data',
   ];
   const longitude = [
-    models.sequelize.literal("ST_X(ST_SnapToGrid(location, 0.0001))"),
-    "longitude",
+    models.sequelize.literal('ST_X(ST_SnapToGrid(location, 0.0001))'),
+    'longitude',
   ];
   const latitude = [
-    models.sequelize.literal("ST_Y(ST_SnapToGrid(location, 0.0001))"),
-    "latitude",
+    models.sequelize.literal('ST_Y(ST_SnapToGrid(location, 0.0001))'),
+    'latitude',
   ];
   const date_shot_min = [
     models.sequelize.literal(
@@ -50,7 +50,7 @@ const parseAttributes = (query) => {
         end)
         `
     ),
-    "date_shot_min",
+    'date_shot_min',
   ];
   const date_shot_max = [
     models.sequelize.literal(
@@ -60,7 +60,7 @@ const parseAttributes = (query) => {
         else date(date_shot_max)::TEXT
         end)`
     ),
-    "date_shot_max",
+    'date_shot_max',
   ];
 
   const default_attributes = [
@@ -76,16 +76,16 @@ const parseAttributes = (query) => {
   }
 
   let attributes = intersection(query.attributes, basic_attributes);
-  if (query.attributes.includes("longitude")) {
+  if (query.attributes.includes('longitude')) {
     attributes.push(longitude);
   }
-  if (query.attributes.includes("latitude")) {
+  if (query.attributes.includes('latitude')) {
     attributes.push(latitude);
   }
-  if (query.attributes.includes("date_shot_min")) {
+  if (query.attributes.includes('date_shot_min')) {
     attributes.push(date_shot_min);
   }
-  if (query.attributes.includes("date_shot_max")) {
+  if (query.attributes.includes('date_shot_max')) {
     attributes.push(date_shot_max);
   }
   return attributes;
@@ -100,21 +100,21 @@ const getImages = async (req, orderkey, count = true) => {
 
   const states = query.state
     ? query.state
-    : ["waiting_validation", "validated"];
+    : ['waiting_validation', 'validated'];
   whereClauses.push({ state: inUniqueOrList(states) });
 
   const isGeoref = states.some((state) =>
-    ["waiting_validation", "validated"].includes(state)
+    ['waiting_validation', 'validated'].includes(state)
   );
 
   switch (query.publish_state) {
-    case "published":
+    case 'published':
       whereClauses.push({ is_published: true });
       break;
-    case "unpublished": {
-      if (!userHasRole(req, "owner_validator", "owner_admin", "super_admin")) {
+    case 'unpublished': {
+      if (!userHasRole(req, 'owner_validator', 'owner_admin', 'super_admin')) {
         throw authorizationError(
-          "Unpublished images can only be accessed by respective owners or super administrators"
+          'Unpublished images can only be accessed by respective owners or super administrators'
         );
       }
       const { where: scopeOwner } = getOwnerScope(req);
@@ -124,14 +124,14 @@ const getImages = async (req, orderkey, count = true) => {
       whereClauses.push(scopeOwner);
       break;
     }
-    case "all": {
+    case 'all': {
       const { where: scopeOwner } = getOwnerScope(req);
-      if (!userHasRole(req, "owner_validator", "owner_admin", "super_admin")) {
+      if (!userHasRole(req, 'owner_validator', 'owner_admin', 'super_admin')) {
         throw authorizationError(
-          "Unpublished images can only be accessed by respective owners or super administrators"
+          'Unpublished images can only be accessed by respective owners or super administrators'
         );
       }
-      if (userHasRole(req, "super_admin")) {
+      if (userHasRole(req, 'super_admin')) {
         // no restriction on is_published - get all images whatever status
       } else {
         // get all published images
@@ -285,7 +285,7 @@ const getImages = async (req, orderkey, count = true) => {
   if (query.only_unlocked) {
     whereClauses.push(
       models.sequelize.literal(
-        "last_start IS NULL OR (EXTRACT(EPOCH FROM now())-EXTRACT(EPOCH FROM last_start))/60 >= 240"
+        'last_start IS NULL OR (EXTRACT(EPOCH FROM now())-EXTRACT(EPOCH FROM last_start))/60 >= 240'
       )
     );
   }
@@ -305,7 +305,7 @@ const getImages = async (req, orderkey, count = true) => {
     },
   };
 
-  const randomOrder = models.sequelize.literal("random()");
+  const randomOrder = models.sequelize.literal('random()');
 
   // findAll returns an array where findAndCountAll returns object {rows, count}
   const response = {
@@ -335,19 +335,19 @@ const getImages = async (req, orderkey, count = true) => {
       );
     }
 
-    const orderById = [["id"]];
+    const orderById = [['id']];
     includeOption = [
       {
         model: models.apriori_locations,
         attributes: [
-          [models.sequelize.literal("ST_X(geom)"), "longitude"],
-          [models.sequelize.literal("ST_Y(geom)"), "latitude"],
-          "exact",
+          [models.sequelize.literal('ST_X(geom)'), 'longitude'],
+          [models.sequelize.literal('ST_Y(geom)'), 'latitude'],
+          'exact',
         ],
         where: whereClauseApriori,
         required: true,
         duplicating: false,
-        order: orderBy === "distance" ? orderByApriori : undefined,
+        order: orderBy === 'distance' ? orderByApriori : undefined,
       },
       includeCollectionFilter,
     ];
@@ -358,9 +358,9 @@ const getImages = async (req, orderkey, count = true) => {
       offset: query.offset || 0,
       where: { [Op.and]: whereClauses },
       order:
-        orderBy === "id"
+        orderBy === 'id'
           ? orderById
-          : orderBy === "random"
+          : orderBy === 'random'
           ? randomOrder
           : orderByApriori,
       include: includeOption,
@@ -371,7 +371,7 @@ const getImages = async (req, orderkey, count = true) => {
       const countPromise = await models.images.count({
         where: { [Op.and]: whereClauses },
         include: includeOption,
-        distinct: "images.id",
+        distinct: 'images.id',
       });
       response.count = countPromise;
       return response;
@@ -383,7 +383,7 @@ const getImages = async (req, orderkey, count = true) => {
     const orderByNearest = models.sequelize.literal(
       `images.location <-> st_setsrid(ST_makepoint(${query.longitude}, ${query.latitude}), 4326)`
     );
-    const orderById = [["id"]];
+    const orderById = [['id']];
     const sequelizeQuery = {
       subQuery: false,
       attributes: attributes,
@@ -391,9 +391,9 @@ const getImages = async (req, orderkey, count = true) => {
       offset: query.offset || 0,
       where: { [Op.and]: whereClauses },
       order:
-        orderBy === "distance"
+        orderBy === 'distance'
           ? orderByNearest
-          : orderBy === "random"
+          : orderBy === 'random'
           ? randomOrder
           : orderById,
       include: [includeCollectionFilter],
@@ -416,18 +416,18 @@ const getImagesFromPOI = async (req) => {
   const orderBy = query.sortKey;
   const poiLocationGeo = Sequelize.cast(
     Sequelize.fn(
-      "ST_SetSRID",
-      Sequelize.fn("ST_MakePoint", query.POI_longitude, query.POI_latitude),
+      'ST_SetSRID',
+      Sequelize.fn('ST_MakePoint', query.POI_longitude, query.POI_latitude),
       4326
     ),
-    "geography"
+    'geography'
   );
   const imageLocationGeo = Sequelize.cast(
-    Sequelize.col("images.location"),
-    "geography"
+    Sequelize.col('images.location'),
+    'geography'
   );
   const distanceExpr = Sequelize.fn(
-    "ST_Distance",
+    'ST_Distance',
     imageLocationGeo,
     poiLocationGeo
   );
@@ -507,7 +507,7 @@ const getImagesFromPOI = async (req) => {
   whereClauses.push(
     Sequelize.where(
       Sequelize.fn(
-        "ST_DWithin",
+        'ST_DWithin',
         imageLocationGeo,
         poiLocationGeo,
         query.POI_MaxDistance
@@ -528,11 +528,11 @@ const getImagesFromPOI = async (req) => {
     attributes: attributes,
     where: { [Op.and]: whereClauses },
     order:
-      orderBy === "distance"
+      orderBy === 'distance'
         ? [orderByNearest]
-        : orderBy === "title"
-        ? [["title"]]
-        : [["date_shot_min"]],
+        : orderBy === 'title'
+        ? [['title']]
+        : [['date_shot_min']],
     limit: query.limit || 30,
     offset: query.offset || 0,
   };
@@ -550,7 +550,7 @@ exports.getList = utils.route(async (req, res) => {
     images = await getImages(req);
   }
   //Build media
-  if (!req.query.attributes || req.query.attributes.includes("media")) {
+  if (!req.query.attributes || req.query.attributes.includes('media')) {
     //only return media if no specific attributes requested or if media requested
     if (images.rows) {
       await mediaUtils.setListImageUrl(
@@ -568,8 +568,8 @@ exports.getList = utils.route(async (req, res) => {
 });
 
 exports.getListId = utils.route(async (req, res) => {
-  req.query = { ...req.query, attributes: ["id"] };
-  const images = await getImages(req, /*orderBy*/ "id", /*count*/ false);
+  req.query = { ...req.query, attributes: ['id'] };
+  const images = await getImages(req, /*orderBy*/ 'id', /*count*/ false);
   // Send flattened objects
   res.status(200).send(images.rows.map((obj) => obj.id));
 });
@@ -584,7 +584,7 @@ exports.getListMetadata = utils.route(async (req, res) => {
     // A super administrator can request image metadata for any owner (or access
     // all images).
     ownerIds = requestedOwnerIds;
-  } else if (user.hasRole("owner_admin") || user.hasRole("owner_validator")) {
+  } else if (user.hasRole('owner_admin') || user.hasRole('owner_validator')) {
     // An owner administrator or validator can only request image metadata for
     // their owner.
     const accessibleOwnerIds = [user.owner_id];
@@ -593,14 +593,14 @@ exports.getListMetadata = utils.route(async (req, res) => {
       : accessibleOwnerIds;
     if (requestedOwnerIds && ownerIds.length !== requestedOwnerIds.length) {
       throw authorizationError(
-        "An owner validator or administrator can only access metadata for images linked to the same owner"
+        'An owner validator or administrator can only access metadata for images linked to the same owner'
       );
     }
   } else {
     throw new Error(
       `Cannot determine owner scope for user ${
         user.id
-      } with unsupported role(s) ${user.roles.join(", ")}`
+      } with unsupported role(s) ${user.roles.join(', ')}`
     );
   }
 
@@ -623,37 +623,37 @@ exports.getListMetadata = utils.route(async (req, res) => {
       model: models.geolocalisations,
       attributes: [
         [
-          models.sequelize.literal("st_X(geolocalisation.location)"),
-          "longitude",
+          models.sequelize.literal('st_X(geolocalisation.location)'),
+          'longitude',
         ],
         [
-          models.sequelize.literal("st_Y(geolocalisation.location)"),
-          "latitude",
+          models.sequelize.literal('st_Y(geolocalisation.location)'),
+          'latitude',
         ],
         [
-          models.sequelize.literal("st_Z(geolocalisation.location)"),
-          "altitude",
+          models.sequelize.literal('st_Z(geolocalisation.location)'),
+          'altitude',
         ],
-        "azimuth",
-        "tilt",
-        "roll",
-        "focal",
+        'azimuth',
+        'tilt',
+        'roll',
+        'focal',
         [
-          models.sequelize.literal("st_AsText(geolocalisation.location)"),
-          "point",
+          models.sequelize.literal('st_AsText(geolocalisation.location)'),
+          'point',
         ],
         [
-          models.sequelize.literal("st_AsText(geolocalisation.footprint)"),
-          "footprint",
+          models.sequelize.literal('st_AsText(geolocalisation.footprint)'),
+          'footprint',
         ],
       ],
       where: {
         [Op.and]: [
           {
-            [Op.or]: [{ state: "validated" }, { state: "improved" }],
+            [Op.or]: [{ state: 'validated' }, { state: 'improved' }],
           },
           {
-            id: { [Op.col]: "images.geolocalisation_id" },
+            id: { [Op.col]: 'images.geolocalisation_id' },
           },
         ],
       },
@@ -671,27 +671,27 @@ exports.getListMetadata = utils.route(async (req, res) => {
   });
 
   const attributes = [
-    "id",
-    "original_id",
-    "collection_id",
-    "owner_id",
-    "state",
-    "title",
-    "caption",
-    "link",
-    "is_published",
-    [models.sequelize.literal("ST_X(images.location)"), "longitude"],
-    [models.sequelize.literal("ST_Y(images.location)"), "latitude"],
+    'id',
+    'original_id',
+    'collection_id',
+    'owner_id',
+    'state',
+    'title',
+    'caption',
+    'link',
+    'is_published',
+    [models.sequelize.literal('ST_X(images.location)'), 'longitude'],
+    [models.sequelize.literal('ST_Y(images.location)'), 'latitude'],
   ];
 
   if (req.query.geolocalisation) {
     attributes.push([
-      Sequelize.col("geometadatum.toponyms_array"),
-      "geotags_array",
+      Sequelize.col('geometadatum.toponyms_array'),
+      'geotags_array',
     ]);
     attributes.push([
-      Sequelize.col("geometadatum.toponyms_json"),
-      "geotags_json",
+      Sequelize.col('geometadatum.toponyms_json'),
+      'geotags_json',
     ]);
   }
 
@@ -700,11 +700,11 @@ exports.getListMetadata = utils.route(async (req, res) => {
     limit: req.query.limit || 30,
     offset: req.query.offset || 0,
     where: cleanedWhere,
-    order: [["id"]],
+    order: [['id']],
     include: req.query.geolocalisation ? includeGeoloc : null,
   });
 
-  res.header("Total-Items", countTotal).status(200).send(images);
+  res.header('Total-Items', countTotal).status(200).send(images);
 });
 
 exports.getStats = utils.route(async (req, res) => {
@@ -762,13 +762,13 @@ exports.getStats = utils.route(async (req, res) => {
   const cleanedWhere = cleanProp(whereImages);
 
   const queryPromise = models.images.findAll({
-    attributes: ["state", [models.sequelize.fn("COUNT", "id"), "count"]],
+    attributes: ['state', [models.sequelize.fn('COUNT', 'id'), 'count']],
     where: cleanedWhere,
-    group: ["state"],
+    group: ['state'],
   });
 
   const result = await utils.handlePromise(queryPromise, {
-    message: "Images cannot be retrieved.",
+    message: 'Images cannot be retrieved.',
   });
 
   res.status(200).send({
